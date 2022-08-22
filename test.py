26ce5f1023740e62cf13fe281b357e68f6a0317a
x_lows <775375798@qq.com>
2022-08-22 20:47:33 +0800

    忽略文件配置起效了

--- test.py
+++ test.py
@@ -5,12 +5,3 @@ import yaml
 
 result = '{"return_code":"SUCCESS","return_msg":{"taskId":88,"checkErrNum":0}}'
 data = re.search('{"taskId":(.+?),', result)
-# with open('testcases/userManage_load_adf.yaml', encoding='utf-8') as f:
-#     data2 = yaml.full_load(f.read())
-# print("data2=", data2[0]["extract"]["taskId"])
-# value = data2[0]["extract"]["taskId"]
-print(data)
-print(data.group(1))
-# data3 = re.search(value, result)
-# print(data3)
-
