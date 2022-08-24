34127aeb676ddd5651753958a7c23053c2ed00e3
1991chenyong <775375798@qq.com>
2022-08-24 21:44:46 +0800

    数据同步3

--- api/test_user/test_user.py
+++ api/test_user/test_user.py
@@ -16,7 +16,7 @@ class TestUser:
     @pytest.mark.skip("用户管理列表接口跳过")
     @allure.story("用例管理列表搜索功能测试用例")
     @pytest.mark.run(order=1)
-    @pytest.mark.parametrize('testcases_info', read_testcase_yaml('testcases/userManage.yaml'))
+    @pytest.mark.parametrize('testcases_info', read_testcase_yaml('testcases/userManage/userManage.yaml'))
     def test_userManage(self, testcases_info):
         allure.dynamic.title(testcases_info["name"])
         RequestUtil().analysis_yaml(testcases_info)
@@ -24,14 +24,14 @@ class TestUser:
     # @pytest.mark.skip("导出接口跳过")
     @allure.story("测试用例管理列表导出功能")
     @pytest.mark.run(order=2)
-    @pytest.mark.parametrize('testcases_info', read_testcase_yaml('testcases/userManage_export.yaml'))
+    @pytest.mark.parametrize('testcases_info', read_testcase_yaml('testcases/userManage/userManage_export.yaml'))
     def test_userManage_export(self, testcases_info):
         allure.dynamic.title(testcases_info["name"])
         RequestUtil().analysis_yaml(testcases_info)
 
     @pytest.mark.skip("单个发放ADFun接口跳过")
     @pytest.mark.run(order=3)
-    @pytest.mark.parametrize('testcases_info', read_testcase_yaml('testcases/userManage_giveadf.yaml'))
+    @pytest.mark.parametrize('testcases_info', read_testcase_yaml('testcases/userManage/userManage_giveadf.yaml'))
     def test_userManage_giveadf(self, testcases_info):
         allure.dynamic.title(testcases_info["name"])
         RequestUtil().analysis_yaml(testcases_info)
@@ -39,7 +39,7 @@ class TestUser:
     @pytest.mark.skip("批量上传adf接口跳过")
     @allure.story("批量上传adf接口")
     @pytest.mark.run(order=4)
-    @pytest.mark.parametrize('testcases_info', read_testcase_yaml('testcases/userManage_load_adf.yaml'))
+    @pytest.mark.parametrize('testcases_info', read_testcase_yaml('testcases/userManage/userManage_load_adf.yaml'))
     def test_userManage_load_adf(self, testcases_info):
         print("测试用例: ", testcases_info)
         allure.dynamic.title(testcases_info["name"])
